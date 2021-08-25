91964ae3e91af9edcf1ef905e62813a33c142200
Flare26 <47835828+Flare26@users.noreply.github.com>
2021-08-25 01:21:08 -0500

    Player Audio

---
+++ Assets/Audio/MusicManager.gd
@@ -0,0 +1,52 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var file_paths = ["Assets/Audio/menu sketch 1.ogg", "Assets/Audio/mii.ogg"]
+var streams = Array()
+var idx_song
+var curr_vol
+var max_vol
+var streamplayer
+var rocketing = false # bool
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("\nloading music...")
+	streamplayer = get_child(0)
+	for i in range(file_paths.size()):
+		if File.new().file_exists(file_paths[i]):
+			print("...found %s [%s]" % [file_paths[i], String(i)])
+			var audio = load(file_paths[i]) 
+			streams.append(audio) # streams appends the loaded file stream
+			
+	PlayMusic(0)
+
+
+func PlayMusic(idx):
+	idx_song = idx
+	streamplayer.stream = streams[idx]
+	streamplayer.play()
+
+# unhandled input function is called like late update in Unity
+
+func _unhandled_input(event):
+	if event.is_action_pressed("vUp"):
+		print("vol++")
+		streamplayer.volume_db += 4
+		
+	if event.is_action_pressed("vDown"):
+		print("vol--")
+		streamplayer.volume_db -= 4
+		
+	if event.is_action_pressed("CycleMusic"):
+		idx_song += 1
+		if idx_song > streams.size() - 1:
+			idx_song = 0
+		PlayMusic(idx_song)
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Assets/Audio/PlayerAudioManager.gd
@@ -0,0 +1,74 @@
+extends Node
+
+var jet_fly
+var jet_start
+
+var player # player node
+
+# audio stream players
+var jetpackplayer
+var gunplayer
+
+var player_velocity
+var isAirborn
+var isRocketing
+var file_paths = ["Assets/Audio/jet_engage.ogg", "Assets/Audio/jet_fly.ogg", "Assets/Audio/shoot_grenade.ogg"]
+var streams = Array()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	print("\nloading player audio...")
+	
+	jetpackplayer = get_child(0)
+	gunplayer = get_child(1)
+	
+	for i in range(file_paths.size()):
+		if File.new().file_exists(file_paths[i]):
+			print("...found %s [%s]" % [file_paths[i], String(i)])
+			var audio = load(file_paths[i]) 
+			streams.append(audio)
+
+
+func PlayJetEngage(jetpackplayer):
+	jetpackplayer.stream = streams[0]
+	jetpackplayer.play()
+
+func PlayJetFly(jetpackplayer):
+	if isRocketing:
+		jetpackplayer.stream = streams[1]
+		jetpackplayer.play()
+
+func PlayJetDisengage(jetpackplayer):
+	pass
+func PlayFireGrenade(gunplayer):
+	
+	gunplayer.stream = streams[2]
+	gunplayer.stream.loop = false
+	gunplayer.play()
+
+func _get_input():
+			#jetpack sounds
+	if Input.is_action_just_pressed("engage_jetpack"):
+		
+		PlayJetEngage(jetpackplayer)
+		isRocketing = true
+		PlayJetFly(jetpackplayer)
+	
+
+	
+	if Input.is_action_just_released("engage_jetpack"):
+		jetpackplayer.stop()
+		isRocketing = false
+		
+	
+	if Input.is_action_just_pressed("fire_grenade") && Input.is_action_pressed("aim_down_sights"):
+		PlayFireGrenade(gunplayer)
+	
+	# if the gunplayer is not stopped it will stay in the 'play' state and loop the gun sound
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_get_input()
